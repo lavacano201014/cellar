16e8051557f59c064ee1e11682f38d23a821c9fb
Nicholas O'Connor <nrfoconnor@gmail.com>
2017-03-12 22:33:42 -0700

    actually using and building it

--- src/cellar.cpp
+++ src/cellar.cpp
@@ -1,28 +1,21 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
 
-namespace fs = boost::filesystem;
+#include "fs.hpp"
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
-    fs::path cwd(fs::current_path());
-	cwd = fs::system_complete(".");
-    fs::directory_iterator iter_end;
-
-    for (fs::directory_iterator iter_cwd(cwd); iter_cwd != iter_end; ++iter_cwd) {
-        try {
-            std::string item = iter_cwd->path().filename().native();
-
-            std::cout << item;
-            std::cout << " ";
-        }
-        catch (const std::exception& exc) {
-            std::cout << "[1;31mfuck[0m" << std::endl;
-            return 1;
-        }
-    }
-    std::cout << std::endl;
+	vector<string> homedir = cellar::fs::listdir(getenv("HOME"));
+	for (string item : homedir) {
+		cout << item;
+		cout << " ";
+	}
+	cout << endl;
     return 0;
 }
