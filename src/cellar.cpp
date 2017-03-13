e30cddee84075216d0f8f3aaaba0e88421cec9f4
Nicholas O'Connor <nrfoconnor@gmail.com>
2017-03-12 23:10:04 -0700

    .gitignore, beginning of cellar.json support

--- src/cellar.cpp
+++ src/cellar.cpp
@@ -1,20 +1,44 @@
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
+#include "json.hpp"
 
 #include "fs.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 int main(int argc, char* argv[]) {
-	vector<string> homedir = cellar::fs::listdir(getenv("HOME"));
+	string homepath = getenv("HOME");
+	vector<string> homedir = cellar::fs::listdir(homepath);
 	for (string item : homedir) {
-		cout << item;
-		cout << " ";
+		if (item.substr(0,5) == ".wine") {
+			cout << item;
+			cout << " ";
+
+			string jsonpath = homepath + "/" + item + "/cellar.json";
+			if (boost::filesystem::exists(jsonpath)) {
+				try {
+					json config;
+					ifstream configstream(jsonpath);
+					stringstream sstr;
+					sstr << configstream.rdbuf();
+					config = json::parse(sstr.str());
+	
+					cout << "has a json" << endl;
+				}
+				catch (const exception &exc) {
+					cout << "- bogus cellar.json file" << endl;
+				}
+			}
+			else { cout << "- anonymous wine bottle" << endl; }
+		}
 	}
 	cout << endl;
     return 0;
