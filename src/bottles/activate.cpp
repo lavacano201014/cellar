13cda029e79ed263be070d681771f534003119b7
Nicholas O'Connor <nrfoconnor@gmail.com>
2017-03-23 15:03:02 -0700

    beginnings of an activate command

---
+++ src/bottles/activate.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "bottles.hpp"
+#include "internal/bottles.hpp"
+
+using namespace std;
+
+void cellar::bottles::switch_active_bottle(int argc, char** argv) {
+    cout << argc << endl;
+}
