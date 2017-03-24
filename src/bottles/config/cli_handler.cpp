04f2c46def8628cf26ea09ae279c1299916a6a4e
Nicholas O'Connor <nrfoconnor@gmail.com>
2017-03-24 15:57:49 -0700

    beginnings of a config command

---
+++ src/bottles/config/cli_handler.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+#include "json.hpp"
+
+#include "bottles.hpp"
+#include "internal/bottles.hpp"
+#include "output.hpp"
+
+using namespace std;
+using namespace cellar;
+using namespace cellar::bottles;
+
+using json = nlohmann::json;
+
+void cellar::bottles::config_command(int argc, vector<string> argv) {
+    if (argc == 1) {
+        output::error("not enough arguments");
+        return;
+    }
+    string command = argv[1];
+
+    output::statement(command);
+}
